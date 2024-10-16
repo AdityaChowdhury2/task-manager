d9639351dcdc92048b674a72d39eb1655bec93aa
AdityaChowdhury2 <adityac5102@gmail.com>
2024-10-16 10:41:15 +0600

    feat: client and server

--- src/hooks/useAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProviders';
-
-const useAuth = () => {
-	const authContext = useContext(AuthContext);
-	return authContext;
-};
-
-export default useAuth;
--- src/utils/formatDate.js
+++
@@ -1,6 +0,0 @@
-import { format } from "date-fns";
-const formatDate = (date) => {
-    const result = format(new Date(date), 'do MMM, yyyy')
-    return result
-}
-export default formatDate
